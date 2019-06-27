ba6a0e3705bf81ae10801a255e750d4107e2ee1c
jqwei3 <jawei3@iflytek.com>
2019-06-27 08:23:23 +0800

    建造者模式

---
+++ app/src/main/java/com/example/weijianqiang/stragdy/jianzaozhe/BuilderActivity.java
@@ -0,0 +1,13 @@
+package com.example.weijianqiang.stragdy.jianzaozhe;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class BuilderActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_builder);
+    }
+}
---
+++ app/src/main/java/com/example/weijianqiang/stragdy/jianzaozhe/Computer.java
@@ -0,0 +1,9 @@
+package com.example.weijianqiang.stragdy.jianzaozhe;
+
+/**
+ * Created by weijianqiang
+ * On 2019/6/26
+ * Description:
+ */
+public class Computer {
+}
